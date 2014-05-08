50b45a27714069360e214112e1522289001faff2
Charlie Powell <powellcj12@gmail.com>
2014-05-07 20:54:43 -0700

    Fun with functions, parameters, and loops

--- src/Brave.java
+++ src/Brave.java
@@ -1,7 +1,12 @@
 public class Brave {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		multiPrint("Hello, world!", 10);
 	}
 
+	public static void multiPrint(String string, int num) {
+		for(int i = 0; i < num; i++) {
+			System.out.println(string);
+		}
+	}
 }
