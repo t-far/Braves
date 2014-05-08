25faee3479bb902daee899f623312d2604eec318
Charlie Powell <powellcj12@gmail.com>
2014-05-07 20:57:27 -0700

    Fun with classes

--- src/Brave.java
+++ src/Brave.java
@@ -1,12 +1,7 @@
 public class Brave {
 
 	public static void main(String[] args) {
-		multiPrint("Hello, world!", 10);
-	}
-
-	public static void multiPrint(String string, int num) {
-		for(int i = 0; i < num; i++) {
-			System.out.println(string);
-		}
+		LinePrinter lp = new LinePrinter("Hello, world!");
+		lp.multiPrint(10);
 	}
 }
---
+++ src/LinePrinter.java
@@ -0,0 +1,17 @@
+public class LinePrinter {
+	private String string;
+
+	public LinePrinter(String string) {
+		this.string = string;
+	}
+
+	public String getString() {
+		return string;
+	}
+	
+	public void multiPrint(int num) {
+		for(int i = 0; i < num; i++) {
+			System.out.println(string);
+		}
+	}
+}
